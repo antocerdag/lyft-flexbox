af541f86ed516a3d7bb20f3c0ef180497c373ea1
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-04 13:14:45 -0400

    footer listo y seccion informacion lista

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,8 @@
-main.js
+(function scroll(){
+	var navBarNuevo = document.getElementsByClassName("navDos");
+	var distscroll= window.pageYOffset || document.documentElement.scrollTop;
+	if (distscroll > 300 ){
+		//alert("hola");
+		navBarNuevo.classList.remove("hide");
+	}
+})();
